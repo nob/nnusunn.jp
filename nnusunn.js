551511ba6a4661f82816edac60fcf55f2fb47e4b
Nob Y <noboruthedragon@gmail.com>
2012-08-17 18:22:02 +0900

    Fixed.

--- nnusunn.js
+++ nnusunn.js
@@ -3,11 +3,7 @@ $(document).ready(function(){
         $(this).find('.tags a').each(function(i){
             tag = $(this).text();
             if(tag === 'fav' || tag === 'favs') {
-                if (location.pathname.match(/post/i)) {
-                    $(this).parentsUntil('.post').siblings().first().find('.fav-icon').show();        
-                }
-                    $(this).parentsUntil('.post').prev().find('.fav-icon').show();        
-                }
+                $(this).parentsUntil('.post').siblings().find('.fav-icon').show();        
                 return false;
             }
         });
