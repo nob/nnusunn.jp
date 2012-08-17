d1b6fda2f9b5da40169970a11fd4cb9c9abfe174
Nob Y <noboruthedragon@gmail.com>
2012-08-17 16:37:55 +0900

    Fixed js syntax.

--- nnusunn.js
+++ nnusunn.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.post').each(function(i){
         $(this).find('.tags a').each(function(i){
             tag = $(this).text();
-            if(tag === 'fav' or tag === 'favs') {
+            if(tag === 'fav' || tag === 'favs') {
                 $(this).parentsUntil('.post').find('.fav-icon').show();        
                 return false;
             }
