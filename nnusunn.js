b858e9bdcc52a1ef0c567da04efc5bf4b8141dbb
Nob Y <noboruthedragon@gmail.com>
2012-08-17 16:27:18 +0900

    Implemented displaying fav icon.

--- nnusunn.js
+++ nnusunn.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    console.log($('.post .tags a').text);
+    $('.post').each(function(i){
+        $(this).find('.tags a').each(function(i){
+            tag = $(this).text();
+            if(tag === 'fav' or tag === 'favs') {
+                $(this).parentsUntil('.post').find('.fav-icon').show();        
+                return false;
+            }
+        });
+    });
 });
